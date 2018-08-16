60c2057b875804906723b335c3530fa8c38d77be
regirock365 <hemedi@protonmail.com>
2018-08-16 10:40:21 +0930

    eslint

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000"
+  uri: "https://project-groceries-graphql-dev.herokuapp.com/"
 });
 
 const authLink = setContext((_, { headers }) => {
