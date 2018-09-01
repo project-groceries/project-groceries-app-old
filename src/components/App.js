aaf841bd54f9a98d2d555dc62763b705542ef853
regirock365 <hemedi@protonmail.com>
2018-09-01 15:16:29 +0930

    offline state detected and user is warned

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,7 @@ import Overview from "./Overview";
 import Classes from "./Classes";
 import Orders from "./Orders";
 import Ingredients from "./Ingredients";
+import { Offline } from "react-detect-offline";
 
 const bodyWrapper = css`
   height: 100vh;
@@ -64,6 +65,17 @@ const menuSection = css`
   }
 `;
 
+const offlineBanner = css`
+  position: absolute;
+  background-color: #ff9e36;
+  padding: 10px;
+  width: 250px;
+  bottom: 10px;
+  right: calc(50% - 125px);
+  border-radius: 10px;
+  text-align: center;
+`;
+
 class App extends Component {
   static propTypes = {
     cookies: instanceOf(Cookies).isRequired
@@ -90,150 +102,159 @@ class App extends Component {
   render() {
     const { cookies } = this.props;
 
-    return this.state.hasToken ? (
-      <div className={bodyWrapper}>
-        <Query query={USER_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading)
-              return (
-                <div
-                  className="flex-center column"
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100vw",
-                    height: "100vh"
-                  }}
-                >
-                  <img
-                    src={logo}
-                    alt="Project Groceries"
-                    height="80px"
-                    className={css`
-                      transform: translateY(30px);
-                    `}
-                  />
-                  <SamsungTVLoader />
-                </div>
-              );
-            if (error) return <div>Error</div>;
+    return (
+      <Fragment>
+        <Offline>
+          <div className={offlineBanner}>
+            Offline: Changes made now may not be saved
+          </div>
+        </Offline>
+        {this.state.hasToken ? (
+          <div className={bodyWrapper}>
+            <Query query={USER_QUERY}>
+              {({ loading, error, data }) => {
+                if (loading)
+                  return (
+                    <div
+                      className="flex-center column"
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100vw",
+                        height: "100vh"
+                      }}
+                    >
+                      <img
+                        src={logo}
+                        alt="Project Groceries"
+                        height="80px"
+                        className={css`
+                          transform: translateY(30px);
+                        `}
+                      />
+                      <SamsungTVLoader />
+                    </div>
+                  );
+                if (error) return <div>Error</div>;
 
-            const {
-              name,
-              type,
-              hasDeclaredAccountType,
-              school,
-              classes,
-              enrolledIn
-            } = data.user;
-            const hasCompletedDetails = Boolean(
-              school && hasDeclaredAccountType
-            );
-            const userClasses = type === "TEACHER" ? classes : enrolledIn;
+                const {
+                  name,
+                  type,
+                  hasDeclaredAccountType,
+                  school,
+                  classes,
+                  enrolledIn
+                } = data.user;
+                const hasCompletedDetails = Boolean(
+                  school && hasDeclaredAccountType
+                );
+                const userClasses = type === "TEACHER" ? classes : enrolledIn;
 
-            return (
-              <Fragment>
-                <section
-                  style={{
-                    backgroundColor: "rgba(128, 128, 128, 0.1)",
-                    transition: "all 0.6s ease",
-                    filter: hasCompletedDetails ? "none" : "blur(8px)",
-                    pointerEvents: hasCompletedDetails ? "auto" : "none"
-                  }}
-                >
-                  <div
-                    className="bar"
-                    style={{ backgroundColor: "rgba(0,0,0,0.1)" }}
-                  >
-                    <Link to="/">
-                      <Logo />
-                    </Link>
-                    <h1>{name}</h1>
-                  </div>
-
-                  <div className={menuSection}>
-                    <Link to="/classes">
-                      <div className="menuSectionTitle">
-                        <Group />
-                        <h3>Classes</h3>
+                return (
+                  <Fragment>
+                    <section
+                      style={{
+                        backgroundColor: "rgba(128, 128, 128, 0.1)",
+                        transition: "all 0.6s ease",
+                        filter: hasCompletedDetails ? "none" : "blur(8px)",
+                        pointerEvents: hasCompletedDetails ? "auto" : "none"
+                      }}
+                    >
+                      <div
+                        className="bar"
+                        style={{ backgroundColor: "rgba(0,0,0,0.1)" }}
+                      >
+                        <Link to="/">
+                          <Logo />
+                        </Link>
+                        <h1>{name}</h1>
                       </div>
-                    </Link>
-                    {userClasses.length ? (
-                      userClasses.map(c => (
-                        <NavLink
-                          key={c.id}
-                          to={`/classes/${c.id}`}
-                          activeStyle={{ backgroundColor: "blue" }}
-                        >
-                          {c.name}
-                        </NavLink>
-                      ))
-                    ) : (
-                      <small>There are no classes... yet</small>
-                    )}
-                  </div>
 
-                  <div className={menuSection}>
-                    <Link to="/orders">
-                      <div className="menuSectionTitle">
-                        <List />
-                        <h3>Orders</h3>
+                      <div className={menuSection}>
+                        <Link to="/classes">
+                          <div className="menuSectionTitle">
+                            <Group />
+                            <h3>Classes</h3>
+                          </div>
+                        </Link>
+                        {userClasses.length ? (
+                          userClasses.map(c => (
+                            <NavLink
+                              key={c.id}
+                              to={`/classes/${c.id}`}
+                              activeStyle={{ backgroundColor: "blue" }}
+                            >
+                              {c.name}
+                            </NavLink>
+                          ))
+                        ) : (
+                          <small>There are no classes... yet</small>
+                        )}
                       </div>
-                    </Link>
-                  </div>
 
-                  <div className={menuSection}>
-                    <Link to="/ingredients">
-                      <div className="menuSectionTitle">
-                        <Cart />
-                        <h3>Ingredients</h3>
+                      <div className={menuSection}>
+                        <Link to="/orders">
+                          <div className="menuSectionTitle">
+                            <List />
+                            <h3>Orders</h3>
+                          </div>
+                        </Link>
                       </div>
-                    </Link>
-                  </div>
 
-                  <input
-                    type="button"
-                    value="Sign Out"
-                    onClick={() => cookies.remove("token")}
-                    style={{
-                      position: "absolute",
-                      bottom: "10px",
-                      left: "10px"
-                    }}
-                  />
-                </section>
-                <main>
-                  {school ? (
-                    hasDeclaredAccountType ? (
-                      <Switch>
-                        <Route exact path="/" component={Overview} />
-                        <Route exact path="/classes" component={Classes} />
-                        <Route exact path="/orders" component={Orders} />
-                        <Route
-                          exact
-                          path="/ingredients"
-                          component={Ingredients}
-                        />
-                        <Route render={() => <Redirect to="/" />} />
-                      </Switch>
-                    ) : (
-                      <DeclareAccountType />
-                    )
-                  ) : (
-                    <NoSchool />
-                  )}
-                </main>
-              </Fragment>
-            );
-          }}
-        </Query>
-      </div>
-    ) : (
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route render={() => <Redirect to="/login" />} />
-      </Switch>
+                      <div className={menuSection}>
+                        <Link to="/ingredients">
+                          <div className="menuSectionTitle">
+                            <Cart />
+                            <h3>Ingredients</h3>
+                          </div>
+                        </Link>
+                      </div>
+
+                      <input
+                        type="button"
+                        value="Sign Out"
+                        onClick={() => cookies.remove("token")}
+                        style={{
+                          position: "absolute",
+                          bottom: "10px",
+                          left: "10px"
+                        }}
+                      />
+                    </section>
+                    <main>
+                      {school ? (
+                        hasDeclaredAccountType ? (
+                          <Switch>
+                            <Route exact path="/" component={Overview} />
+                            <Route exact path="/classes" component={Classes} />
+                            <Route exact path="/orders" component={Orders} />
+                            <Route
+                              exact
+                              path="/ingredients"
+                              component={Ingredients}
+                            />
+                            <Route render={() => <Redirect to="/" />} />
+                          </Switch>
+                        ) : (
+                          <DeclareAccountType />
+                        )
+                      ) : (
+                        <NoSchool />
+                      )}
+                    </main>
+                  </Fragment>
+                );
+              }}
+            </Query>
+          </div>
+        ) : (
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route render={() => <Redirect to="/login" />} />
+          </Switch>
+        )}
+      </Fragment>
     );
   }
 }
