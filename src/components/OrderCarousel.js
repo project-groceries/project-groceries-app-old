b37e1b96a2c5e3f131a65616e040b6aed8602136
regirock365 <hemedi@protonmail.com>
2018-11-01 15:05:17 +1030

    async order, getting rid of jank

--- src/components/OrderCarousel.js
+++ src/components/OrderCarousel.js
@@ -6,6 +6,7 @@ import { ORDER_CAROUSEL_QUERY, CREATE_ORDERS_MUTATION } from "../queries";
 import Spinner from "./Spinner";
 import { RadioSelect } from "@atlaskit/select";
 import Select from "react-select";
+import AsyncSelect from "react-select/lib/Async";
 import Button from "@atlaskit/button";
 import Modal, { ModalTransition } from "@atlaskit/modal-dialog";
 import styled from "styled-components";
@@ -24,6 +25,17 @@ const BlackClose = styled(Close)`
     transform: scale(2);
   }
 `;
+
+const colourOptions = [
+  { value: "red", label: "red" },
+  { value: "blue", label: "blue" },
+  { value: "green", label: "green" },
+  { value: "yellow", label: "yellow" },
+  { value: "orange", label: "orange" },
+  { value: "indigo", label: "indigo" },
+  { value: "violet", label: "violet" },
+  { value: "teal", label: "teal" }
+];
 
 class OrderCarousel extends Component {
   constructor(props) {
@@ -146,13 +158,39 @@ class OrderCarousel extends Component {
                     />
                     {currentClass && (
                       <Fragment>
-                        <Select
+                        {/* <Select
                           placeholder="Select an ingredient to add to the order"
                           value=""
                           maxMenuHeight={200}
                           options={ingredients
                             .filter(i => !selectedIngredients.has(i.id))
                             .map(i => ({ value: i.id, label: i.name }))}
+                          onChange={data => {
+                            if (data.value) {
+                              this.setState(prev => ({
+                                selectedIngredients: prev.selectedIngredients.set(
+                                  data.value,
+                                  {
+                                    ...data,
+                                    amount: 1
+                                  }
+                                )
+                              }));
+                            }
+                          }}
+                        /> */}
+                        <AsyncSelect
+                          // cacheOptions
+                          value=""
+                          placeholder="Select an ingredient to add to the order"
+                          maxMenuHeight={200}
+                          loadOptions={(inputValue, callback) => {
+                            callback(
+                              this.filterIngredients(inputValue, ingredients)
+                            );
+                          }}
+                          // defaultOptions
+                          onInputChange={this.handleInputChange}
                           onChange={data => {
                             if (data.value) {
                               this.setState(prev => ({
@@ -475,6 +513,22 @@ class OrderCarousel extends Component {
       </div>
     );
   }
+
+  handleInputChange = inputValue => {
+    // const inputValue = newValue.replace(/\W/g, "");
+    this.setState({ inputValue });
+    return inputValue;
+  };
+
+  filterIngredients = (inputValue, ingredients) => {
+    if (inputValue.length > 2) {
+      return ingredients
+        .filter(i => i.name.toLowerCase().includes(inputValue.toLowerCase()))
+        .map(i => ({ value: i.id, label: i.name }));
+    } else {
+      return [];
+    }
+  };
 }
 
 export default OrderCarousel;
